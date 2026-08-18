08ecef449b07f232bb8ab5d094d70825d8f12300
SRao2014 <s.rao_25@thomastallis.org.uk>
2026-08-18 13:34:56 +0100

    main.py

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Memory Test App is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
